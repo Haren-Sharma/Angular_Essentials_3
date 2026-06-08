6240180263e5d4161be6e482b927f8ca1a7da283
Haren-Sharma <harensharma33@gmail.com>
2026-06-08 12:36:49 +0530

    Pipes : Accepting arguments in pipes

--- 3-Pipes/src/app/temperature-pipe.pipe.ts
+++ 3-Pipes/src/app/temperature-pipe.pipe.ts
@@ -5,14 +5,29 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class TemperaturePipe implements PipeTransform {
-  transform(value: string | number, ...args: unknown[]): unknown {
-    let val = 0;
+  transform(
+    value: string | number | null,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah',
+  ): string | null {
+    if (!value) {
+      return null;
+    }
+    let output: string;
+    let symbol = (t: 'cel' | 'fah') => (t === 'cel' ? ' C' : ' F');
+    let val: number;
     if (typeof value === 'string') {
       val = parseFloat(value);
     } else {
       val = value;
     }
-    val=val * 9/5 + 32
-    return val+' F';
+    if (inputType === 'cel' && outputType === 'fah') {
+      output = ((val * 9) / 5 + 32).toFixed(2) + symbol('fah');
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      output = ((val - 32) * (5 / 9)).toFixed(2) + symbol('cel');
+    } else {
+      output = (val).toFixed(2) + symbol(inputType);
+    }
+    return output;
   }
 }
