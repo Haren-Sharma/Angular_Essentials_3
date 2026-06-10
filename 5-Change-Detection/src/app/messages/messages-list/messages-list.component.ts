03368242411486e228d11b4d0fc34fb14541be4a
Haren-Sharma <harensharma33@gmail.com>
2026-06-10 17:42:48 +0530

    Change-Detection : Async Pipe

--- 5-Change-Detection/src/app/messages/messages-list/messages-list.component.ts
+++ 5-Change-Detection/src/app/messages/messages-list/messages-list.component.ts
@@ -6,6 +6,7 @@ import {
   OnInit,
 } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
@@ -13,29 +14,31 @@ import { MessagesService } from '../messages.service';
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [AsyncPipe],
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   constructor(
     private messagesService: MessagesService,
     private cdRef: ChangeDetectorRef,
     private desRef: DestroyRef,
   ) {}
 
-  messages:string[]=[]
+  // messages:string[]=[]
+  messages$ = this.messagesService.messages$;
 
-  ngOnInit(): void {
-    const subscription = this.messagesService.messages$.subscribe(
-      //this method will be executed every time a new value is emitted
-      (messages) => {
-        this.messages=messages
-        this.cdRef.markForCheck(); //manually trigeering change detection 
-      },
-    );
-    this.desRef.onDestroy(() => {
-      //cleanup
-      subscription.unsubscribe();
-    });
-  }
+  // ngOnInit(): void {
+  //   const subscription = this.messagesService.messages$.subscribe(
+  //     //this method will be executed every time a new value is emitted
+  //     (messages) => {
+  //       this.messages=messages
+  //       this.cdRef.markForCheck(); //manually trigeering change detection
+  //     },
+  //   );
+  //   this.desRef.onDestroy(() => {
+  //     //cleanup
+  //     subscription.unsubscribe();
+  //   });
+  // }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
